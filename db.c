ddec5bb8a39ee2fc7b699503485ac3144585e9a7
Methack <45518811+Methack@users.noreply.github.com>
2023-02-08 15:57:24 +0100

    Added pck baseline
    Added baseline for number of packets

--- db.c
+++ db.c
@@ -11,31 +11,33 @@
 #define INSERT_OK 1
 #define INSERT_FAIL 0
 
-int db_insert_baseline(uint64_t timestamp, uint64_t baseline){
+int db_insert_baseline(uint64_t timestamp, uint64_t bt_baseline, uint64_t pk_baseline, uint64_t bt_diff, uint64_t pk_diff){
 	PGresult *res;
 	PGconn *db;
 
 	db = PQconnectdb("dbname=pgnetflowddos user=xjires02");
 	if(PQstatus(db) == CONNECTION_BAD){
-		fprintf(stderr, "Failed to connect to db %s", PQerrorMessage(db));
 		return INSERT_FAIL;
 	}
 	
-	const char sql[] = "INSERT INTO baseline(time, value) VALUES (to_timestamp($1), $2)";
+	const char sql[] = "INSERT INTO baseline(time, bytes, packets, bdiff, pdiff) VALUES (to_timestamp($1), $2, $3, $4, $5)";
 	char btimestamp[11];
 	snprintf(btimestamp, 11, "%"PRIu64, timestamp);
-	uint64_t bbaseline = htonll(baseline);
-	const char * const paramValues[] = {btimestamp, (char *)&bbaseline};
-	const int paramLengths[] = {11, sizeof(uint64_t)};
-	const int paramFormats[] = {0,1};
-
-	res = PQexecParams(db, sql, 2, NULL, paramValues, paramLengths, paramFormats, 0);
+	uint64_t bbaseline = htonll(bt_baseline);
+	uint64_t pbaseline = htonll(pk_baseline);
+	uint64_t bdiff = htonll(bt_diff);
+	uint64_t pdiff = htonll(pk_diff);
+	
+	const char * const paramValues[] = {btimestamp, (char *)&bbaseline, (char *)&pbaseline, (char *)&bdiff, (char *)&pdiff};
+	const int paramLengths[] = {11, sizeof(uint64_t), sizeof(uint64_t), sizeof(uint64_t), sizeof(uint64_t)};
+	const int paramFormats[] = {0, 1, 1, 1, 1};
+	res = PQexecParams(db, sql, 5, NULL, paramValues, paramLengths, paramFormats, 0);
 	if(PQresultStatus(res) != PGRES_COMMAND_OK){
-		fprintf(stderr, "Failed to insert: %s", PQresultErrorMessage(res));
 		return INSERT_FAIL;
 	}
 	PQclear(res);
 
+	
 	PQfinish(db);
 	
 	return INSERT_OK; 
